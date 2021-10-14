77dcf24190405e8f0ea04fa6594ed2a06163a59b
jvaldena <jerome@webetdesign.com>
2021-10-14 16:59:16 +0200

    update composer

--- src/Repository/ParameterRepository.php
+++ src/Repository/ParameterRepository.php
@@ -4,7 +4,7 @@ namespace WebEtDesign\ParameterBundle\Repository;
 
 use WebEtDesign\ParameterBundle\Entity\Parameter;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Parameter|null find($id, $lockMode = null, $lockVersion = null)
